e0f91013c2cfca972170a91a7d89c033219f7782
SomeYellowGuy <30015613+SomeYellowGuy@users.noreply.github.com>
2026-08-26 12:56:57 +0530

    added display nbt loading

--- steel-core/src/entity/entities/objects/display_ui/display.rs
+++ steel-core/src/entity/entities/objects/display_ui/display.rs
@@ -1,32 +1,86 @@
 //! Vanilla's abstract `Display` implementation.
 
-use simdnbt::owned::NbtCompound;
-use simdnbt::borrow::NbtCompound as BorrowedNbtCompoundView;
-use steel_registry::entity_data::{Quaternionf, Vector3f};
+use glam::Mat4;
+use simdnbt::owned::{NbtCompound, NbtTag};
+use simdnbt::borrow::{NbtCompound as BorrowedNbtCompoundView};
+use simdnbt::{FromNbtTag, ToNbtTag};
+use steel_registry::blocks::behavior::PushReaction;
+use steel_registry::entity_data::{Matrix4f, Quaternionf, Vector3f};
 use steel_registry::vanilla_entity_data::DisplayEntityData;
-use steel_utils::BoundingBox;
-use steel_utils::locks::SyncMutex;
 use crate::entity::Entity;
 
+#[repr(i8)]
 #[derive(Debug, Clone, Copy)]
-struct DisplayState {
-    interpolation_duration: i32,
-    last_progress: f32,
-    culling_bounding_box: BoundingBox,
-    update_render_state: bool,
-    update_start_tick: bool,
-    update_interpolation_duration: bool
-}
-
-struct DisplayBase {
-    state: SyncMutex<DisplayState>
-}
-
 pub enum BillboardConstraints {
-    Fixed,
-    Vertical,
-    Horizontal,
-    Center
+    Fixed = 0,
+    Vertical = 1,
+    Horizontal = 2,
+    Center = 3
+}
+
+impl ToNbtTag for BillboardConstraints {
+    fn to_nbt_tag(self) -> NbtTag {
+        NbtTag::String(
+            match self {
+                Self::Fixed => "fixed",
+                Self::Vertical => "vertical",
+                Self::Horizontal => "horizontal",
+                Self::Center => "center"
+            }.into()
+        )
+    }
+}
+
+impl FromNbtTag for BillboardConstraints {
+    fn from_nbt_tag(tag: simdnbt::borrow::NbtTag) -> Option<Self> {
+        match tag.string()?.to_string().as_str() {
+            "fixed" => Some(Self::Fixed),
+            "vertical" => Some(Self::Vertical),
+            "horizontal" => Some(Self::Horizontal),
+            "center" => Some(Self::Center),
+            _ => None
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Brightness {
+    block: i32,
+    sky: i32
+}
+
+impl Brightness {
+    pub fn pack(self) -> i32 {
+        self.block << 4 | self.sky << 20
+    }
+
+    pub fn unpack(bits: i32) -> Brightness {
+        Self {
+            block: (bits >> 4) & 0b1111,
+            sky: (bits >> 20) & 0b1111
+        }
+    }
+}
+
+impl ToNbtTag for Brightness {
+    fn to_nbt_tag(self) -> NbtTag {
+        let mut compound = NbtCompound::new();
+        compound.insert("block", self.block);
+        compound.insert("sky", self.sky);
+        NbtTag::Compound(compound)
+    }
+}
+
+impl FromNbtTag for Brightness {
+    fn from_nbt_tag(tag: NbtTag) -> Option<Self> {
+        let compound = tag.compound()?;
+        let block = compound.get("block")?.int()?;
+        let range = 0..=15;
+        if !range.contains(&block) { return None }
+        let sky = compound.get("sky")?.int()?;
+        if !range.contains(&sky) { return None }
+        Some(Self { block, sky })
+    }
 }
 
 /// A structure describing an affine transformation in 3D space.
@@ -53,6 +107,69 @@ impl Transformation {
         scale: Vector3f::ONE,
         right_rotation: Quaternionf::IDENTITY,
     };
+
+    pub fn compose(self) -> Mat4 {
+        Mat4::from_translation(self.translation.into())
+            * Mat4::from_quat(self.left_rotation.into())
+            * Mat4::from_scale(self.scale.into())
+            * Mat4::from_quat(self.right_rotation.into())
+    }
+}
+
+impl From<Mat4> for Transformation {
+    /// Composes a [`Transformation`] with the provided matrix.
+    fn from(mat: Mat4) -> Self {
+        // TODO: Implement svdDecompose()
+    }
+}
+
+impl From<Transformation> for Mat4 {
+    /// Decomposes a [`Transformation`] to form a matrix.
+    fn from(t: Transformation) -> Self {
+        Mat4::from_translation(t.translation.into())
+            * Mat4::from_quat(t.left_rotation.into())
+            * Mat4::from_scale(t.scale.into())
+            * Mat4::from_quat(t.right_rotation.into())
+    }
+}
+
+struct NormalTransformation(Transformation);
+impl From<Transformation> for NormalTransformation { fn from(t: Transformation) -> Self { Self(t)} }
+impl From<NormalTransformation> for Transformation { fn from(t: NormalTransformation) -> Self { t.0 } }
+
+// Recreates Vanilla's `Transformation.CODEC`.
+impl ToNbtTag for NormalTransformation {
+    fn to_nbt_tag(&self) -> NbtTag {
+        let mut compound = NbtCompound::new();
+        compound.insert("translation", self.0.translation.to_nbt_tag());
+        compound.insert("left_rotation", self.0.left_rotation.to_nbt_tag());
+        compound.insert("scale", self.0.scale.to_nbt_tag());
+        compound.insert("right_rotation", self.0.right_rotation.to_nbt_tag());
+        NbtTag::Compound(compound)
+    }
+}
+
+impl FromNbtTag for NormalTransformation {
+    fn from_nbt_tag(tag: simdnbt::borrow::NbtTag) -> Option<Self> {
+        let compound = tag.compound()?;
+        Some(Self (Transformation {
+            translation: Vector3f::from_nbt_tag(compound.get("transformation")?)?,
+            left_rotation: Quaternionf::from_nbt_tag(compound.get("left_rotation")?)?,
+            scale: Vector3f::from_nbt_tag(compound.get("scale")?)?,
+            right_rotation: Quaternionf::from_nbt_tag(compound.get("right_rotation")?)?,
+        }))
+    }
+}
+
+// Recreates Vanilla's `Transformation.EXTENDED_CODEC`.
+// This codec prefers using the ordinary codec created above, but it also accepts a matrix.
+impl FromNbtTag for Transformation {
+    fn from_nbt_tag(tag: simdnbt::borrow::NbtTag) -> Option<Self> {
+        if let Some(NormalTransformation(transformation)) = NormalTransformation::from_nbt_tag(tag) {
+            return Some(transformation);
+        }
+        Some(Matrix4f::from_nbt_tag(tag)?.into())
+    }
 }
 
 /// The abstract display trait, used by block, item and display entities.
@@ -77,10 +194,28 @@ pub trait Display: Entity {
     }
 
     fn load_additional_display(&self, nbt: BorrowedNbtCompoundView<'_, '_>) {
+        self.set_transformation(Transformation::from_nbt_tag(nbt.list("transformation").into()).unwrap_or_else(|| Transformation::IDENTITY));
 
+        let mut data = self.display_entity_data_mut();
+        data.transformation_interpolation_duration.set(nbt.int("interpolation_duration").unwrap_or(0));
+        data.transformation_interpolation_start_delta_ticks.set(nbt.int("start_interpolation").unwrap_or(0));
+        let teleport_duration = nbt.int("teleport_duration").unwrap_or(0);
+        data.transformation_interpolation_duration.set(teleport_duration.clamp(0, 59));
+        data.billboard_render_constraints.set(nbt.get("billboard").and_then(BillboardConstraints::from_nbt_tag).unwrap_or(BillboardConstraints::Fixed) as i8);
+        data.view_range.set(nbt.float("view_range").unwrap_or(1.0));
+        data.shadow_radius.set(nbt.float("shadow_radius").unwrap_or(0.0));
+        data.shadow_strength.set(nbt.float("shadow_strength").unwrap_or(1.0));
+        data.width.set(nbt.float("width").unwrap_or(0.0));
+        data.height.set(nbt.float("height").unwrap_or(0.0));
+        data.glow_color_override.set(nbt.int("glow_color_override").unwrap_or(-1));
+        data.brightness_override.set(nbt.get("brightness").and_then(Brightness::from_nbt_tag).map(Brightness::unpack).unwrap_or(-1));
     }
 
-    fn save_additional(&self, nbt: &mut NbtCompound) {
-
+    fn save_additional_display(&self, nbt: &mut NbtCompound) {
+        // TODO
     }
+
+    fn hurt_display(&self) -> bool { false }
+    fn piston_push_reaction(&self) -> PushReaction { PushReaction::Ignore }
+    fn is_ignoring_block_triggers(&self) -> bool { true }
 }
---
+++ steel-registry/src/entity_data/math.rs
@@ -0,0 +1,167 @@
+use glam::{Mat4, Quat, Vec3};
+use simdnbt::owned::{NbtCompound, NbtList, NbtTag};
+use simdnbt::{FromNbtTag, ToNbtTag};
+
+type BorrowedNbtTag<'a: 'tape, 'tape> = simdnbt::borrow::NbtTag<'a, 'tape>;
+
+/// A 3D vector (for display entities).
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Vector3f {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+impl Vector3f {
+    pub const ZERO: Self = Self {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+    };
+
+    pub const ONE: Self = Self {
+        x: 1.0,
+        y: 1.0,
+        z: 1.0,
+    };
+
+    #[must_use]
+    pub const fn new(x: f32, y: f32, z: f32) -> Self {
+        Self { x, y, z }
+    }
+}
+
+impl From<Vector3f> for Vec3 {
+    fn from(value: Vector3f) -> Self {
+        Vec3::new(value.x, value.y, value.z)
+    }
+}
+
+impl ToNbtTag for Vector3f {
+    fn to_nbt_tag(self) -> NbtTag {
+        NbtList::Float(vec![self.x, self.y, self.z]).into()
+    }
+}
+
+impl FromNbtTag for Vector3f {
+    fn from_nbt_tag(tag: simdnbt::borrow::NbtTag) -> Option<Self> {
+        if let Some(l) = tag.list() && let Some(floats) = l.floats() && floats.len() == 3 {
+            Some(Vector3f::new(floats[0], floats[1], floats[2]))
+        } else {
+            None
+        }
+    }
+}
+
+/// A rotation storing an angle and axis (in 3 components).
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct AxisAngle4f {
+    pub angle: f32,
+    pub axis: Vector3f
+}
+
+impl ToNbtTag for AxisAngle4f {
+    fn to_nbt_tag(self) -> NbtTag {
+        let mut compound = NbtCompound::new();
+        compound.insert("angle", self.angle);
+        compound.insert("axis", self.axis);
+        NbtTag::Compound(compound)
+    }
+}
+
+impl FromNbtTag for AxisAngle4f {
+    fn from_nbt_tag(tag: BorrowedNbtTag) -> Option<Self> {
+        if let Some(compound) = tag.compound()
+            && let Some(angle) = compound.get("angle")
+            && let Some(axis) = compound.get("axis")
+        {
+            Some(
+                Self {
+                    angle: f32::from_nbt_tag(angle)?,
+                    axis: Vector3f::from_nbt_tag(axis)?
+                }
+            )
+        } else { None }
+    }
+}
+
+/// A quaternion rotation (for display entities).
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Quaternionf {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub w: f32,
+}
+
+/// A quaternion of 4 elements.
+impl Quaternionf {
+    pub const IDENTITY: Self = Self {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+        w: 1.0,
+    };
+
+    #[must_use]
+    pub const fn new(x: f32, y: f32, z: f32, w: f32) -> Self {
+        Self { x, y, z, w }
+    }
+}
+impl From<Quaternionf> for Quat {
+    fn from(value: Quaternionf) -> Self {
+        Quat::from_xyzw(value.x, value.y, value.z, value.w)
+    }
+}
+
+impl From<AxisAngle4f> for Quaternionf {
+    fn from(value: AxisAngle4f) -> Self {
+        let half_angle = value.angle / 2.0;
+        let sin = half_angle.sin();
+        let cos = half_angle.cos();
+        Self {
+            x: value.axis.x * sin,
+            y: value.axis.y * sin,
+            z: value.axis.z * sin,
+            w: cos
+        }
+    }
+}
+
+impl ToNbtTag for Quaternionf {
+    fn to_nbt_tag(self) -> NbtTag {
+        NbtList::Float(vec![self.x, self.y, self.z, self.w]).into()
+    }
+}
+
+impl FromNbtTag for Quaternionf {
+    fn from_nbt_tag(tag: BorrowedNbtTag) -> Option<Self> {
+        // One of the two: 4 floats or AxisAngle4f
+        if let Some(l) = tag.list() && let Some(floats) = l.floats() && floats.len() == 4 {
+            return Some(Quaternionf::new(floats[0], floats[1], floats[2], floats[3]))
+        }
+        Some(AxisAngle4f::from_nbt_tag(tag)?.into())
+    }
+}
+
+/// A 4D matrix (for display entities).
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Matrix4f(Mat4);
+
+impl FromNbtTag for Matrix4f {
+    fn from_nbt_tag(tag: BorrowedNbtTag) -> Option<Self> {
+        let list = tag.list()?;
+        let Some(elements) = list.floats() else {
+            return None;
+        };
+        let elements = elements.into_boxed_slice().as_ref().try_into().ok()?;
+        Some(Matrix4f(Mat4::from_cols_array(&elements).transpose()))
+    }
+}
+
+impl ToNbtTag for Matrix4f {
+    fn to_nbt_tag(self) -> NbtTag {
+        let elements = self.0.transpose().to_cols_array();
+        NbtList::Float(elements.to_vec()).into()
+    }
+}
